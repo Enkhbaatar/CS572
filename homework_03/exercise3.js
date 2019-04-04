bbf6bed3233d9de686e2794edc422d98ad3d3921
Enkhbaatar <enhee.day@gmail.com>
2019-04-04 14:20:23 -0500

    file name changed

--- homework_03/exercise3.js
+++ homework_03/exercise3.js
@@ -4,17 +4,11 @@ const fs = require("fs");
 const start_time = Date.now();
 http
   .createServer(function(req, res) {
-    const largeFilePath = path.join(__dirname, "297MBFile.txt");
-    let fileSync = fs.readFileSync(largeFilePath);
-    //let file = fs.readFile(largeFilePath);
-    const stream = fs.createReadStream(largeFilePath);
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    var obj = {
-      response: "success"
-    };
-    res.end(JSON.stringify(obj), () => {
-      console.log(Date.now() - start_time);
-    });
+    const largeFile = path.join(__dirname, "minecraft.exe");
+    let fileSync = fs.readFileSync(largeFile);
+    //let file = fs.readFile(largeFile);
+    console.log(Date.now() - start_time);
+    res.end(fileSync);
+    //var stream = fs.createReadStream(largeFile).pipe(res);
   })
   .listen(5000, () => console.log("listening on 5000"));
