333e962442ea58a2b24de6c1764ef3c1a2d9d7de
Enkhbaatar <enhee.day@gmail.com>
2019-04-04 13:36:31 -0500

    HW3 exercise3 time added

--- homework_03/exercise3.js
+++ homework_03/exercise3.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-
+const start_time = Date.now();
 http
   .createServer(function(req, res) {
     const largeFilePath = path.join(__dirname, "297MBFile.txt");
@@ -13,6 +13,8 @@ http
     var obj = {
       response: "success"
     };
-    res.end(JSON.stringify(obj));
+    res.end(JSON.stringify(obj), () => {
+      console.log(Date.now() - start_time);
+    });
   })
   .listen(5000, () => console.log("listening on 5000"));
