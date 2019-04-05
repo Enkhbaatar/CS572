52ce86b7c01c66d1661630f01adf610d706e050b
Enkhbaatar <enhee.day@gmail.com>
2019-04-05 15:55:07 -0500

    HW 5, Link pagination, axios

---
+++ homework_05/exercise.js
@@ -0,0 +1,42 @@
+{
+  const express = require("express");
+  const axios = require("axios");
+  const app = express();
+
+  app.set("env", "development");
+  app.enable("trust proxy");
+  app.enable("case sensitive routing");
+  app.set("x-powered-by", false);
+
+  const port = 5000;
+  const dataURL = "https://randomuser.me/api/?results=10";
+
+  app.listen(port, () => {
+    console.log("listening port " + port);
+  });
+
+  //Handle a user get request
+  app.get("/users", (req, res) => {
+    getUser(res);
+  });
+
+  async function getUser(res) {
+    try {
+      const response = await axios.get(dataURL);
+      res.set({
+        "Cache-control": "private, max-age=86400",
+        "Last-Modified": new Date(),
+        Link:
+          `<https://randomuser.me/api/?page=1&results=10>; rel="first",` +
+          `<https://randomuser.me/api/?page=${response.data.info.page +
+            1}&results=${response.data.info.seed}>; rel="next",` +
+          `<https://randomuser.me/api/?page=${response.data.info.page -
+            1}&results=${response.data.info.seed}>; rel="prev",`
+      });
+      console.error(response.data);
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
